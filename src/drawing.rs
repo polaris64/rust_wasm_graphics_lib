92249a03ea4d4a521e33722d90e6a50c6435a295
Simon Pugnet <simon@polaris64.net>
2019-03-22 10:04:02 +0100

    Added Drawing::h_line() and Drawing::v_line()

--- src/drawing.rs
+++ src/drawing.rs
@@ -28,3 +28,47 @@ pub fn fill_rect(c: &mut Canvas, col: &ARGBColour, mut x1: usize, mut y1: usize,
         }
     }
 }
+
+#[wasm_bindgen]
+pub fn h_line(c: &mut Canvas, col: &ARGBColour, mut x1: usize, y: usize, mut x2: usize) {
+    if y >= c.height() {
+        return;
+    }
+    if x1 > x2 {
+        let temp = x1;
+        x1 = x2;
+        x2 = temp;
+    }
+    if x1 > c.width() {
+        return;
+    }
+    if x2 > c.width() { x2 = c.width() }
+    let col: u32 = col.into();
+    let mut idx = c.buffer_index(x1, y);
+    for _ in x1..x2 {
+        c.buffer()[idx] = col;
+        idx += 1;
+    }
+}
+
+#[wasm_bindgen]
+pub fn v_line(c: &mut Canvas, col: &ARGBColour, x: usize, mut y1: usize, mut y2: usize) {
+    if x >= c.width() {
+        return;
+    }
+    if y1 > y2 {
+        let temp = y1;
+        y1 = y2;
+        y2 = temp;
+    }
+    if y1 > c.height() {
+        return;
+    }
+    if y2 > c.height() { y2 = c.height() }
+    let col: u32 = col.into();
+    let mut idx = c.buffer_index(x, y1);
+    for _ in y1..y2 {
+        c.buffer()[idx] = col;
+        idx += c.width()
+    }
+}
--- www/index.js
+++ www/index.js
@@ -2,13 +2,15 @@ import {
   ARGBColour,
   Canvas,
   fill_rect,
+  h_line,
+  v_line,
 } from 'rust-wasm-graphics-lib';
 
 import { memory } from 'rust-wasm-graphics-lib/rust_wasm_graphics_lib_bg';
 
 const SIZE_MULT = 4;
 const clear_colour = ARGBColour.new(255, 0, 128, 0);
-const rect_colour = ARGBColour.new(255, 0, 0, 0);
+const draw_colour = ARGBColour.new(255, 0, 0, 0);
 
 const rust_canvas = Canvas.new(128, 128);
 const height      = rust_canvas.height();
@@ -34,9 +36,58 @@ const demo_colourful_rect = () => {
   nr = Math.floor(((Math.sin(counter * 1.5) + 1) / 2) * 255);
   ng = Math.floor(((Math.cos(counter * 1.7) + 1) / 2) * 255);
   nb = Math.floor(((Math.sin(counter * 1.3) + 1) / 2) * 255);
-  rect_colour.r = nr;
-  rect_colour.g = ng;
-  rect_colour.b = nb;
+  draw_colour.r = nr;
+  draw_colour.g = ng;
+  draw_colour.b = nb;
+
+  let rectx = Math.floor(((Math.sin(counter * 1.3) + 1) / 2) * width  / 2);
+  let recty = Math.floor(((Math.cos(counter * 1.5) + 1) / 2) * height / 2);
+  let rectw = Math.floor(((Math.sin(counter * 2.1) + 1) / 2) * width  / 2) + 16;
+  let recth = Math.floor(((Math.cos(counter * 1.8) + 1) / 2) * height / 2) + 16;
+
+  fill_rect(
+    rust_canvas,
+    draw_colour,
+    rectx,
+    recty,
+    rectx + rectw,
+    recty + recth,
+  );
+
+};
+
+const demo_hlines = () => {
+  let nr = Math.floor(((Math.sin(counter * 1.1) + 1) / 2) * 255);
+  let ng = Math.floor(((Math.cos(counter * 0.8) + 1) / 2) * 255);
+  let nb = Math.floor(((Math.sin(counter * 1.4) + 1) / 2) * 255);
+  draw_colour.r = nr;
+  draw_colour.g = ng;
+  draw_colour.b = nb;
+
+  let x1 = Math.floor(((Math.sin(counter * 0.3) + 1) / 2) * width);
+  let x2 = Math.floor(((Math.sin(counter * 1.1) + 1) / 2) * width) + 16;
+  let y = Math.floor(((Math.cos(counter * 0.5) + 1) / 2) * height);
+
+  h_line(rust_canvas, draw_colour, x1, y, x2);
+};
+
+const demo_vlines = () => {
+  let nr = Math.floor(((Math.sin(counter * 1.2) + 1) / 2) * 255);
+  let ng = Math.floor(((Math.cos(counter * 1.3) + 1) / 2) * 255);
+  let nb = Math.floor(((Math.sin(counter * 1.8) + 1) / 2) * 255);
+  draw_colour.r = nr;
+  draw_colour.g = ng;
+  draw_colour.b = nb;
+
+  let y1 = Math.floor(((Math.sin(counter * 0.5) + 1) / 2) * height);
+  let y2 = Math.floor(((Math.sin(counter * 1.2) + 1) / 2) * height) + 16;
+  let x = Math.floor(((Math.cos(counter * 0.3) + 1) / 2) * width);
+
+  v_line(rust_canvas, draw_colour, x, y1, y2);
+};
+
+const renderLoop = () => {
+  //debugger;
 
   if (counter > Math.PI * 10) {
     let nr = Math.floor(((Math.sin(counter * 10.2) + 1) / 2) * 255);
@@ -50,26 +101,9 @@ const demo_colourful_rect = () => {
     counter = 0;
   }
 
-  let rectx = Math.floor(((Math.sin(counter * 1.3) + 1) / 2) * width  / 2);
-  let recty = Math.floor(((Math.cos(counter * 1.5) + 1) / 2) * height / 2);
-  let rectw = Math.floor(((Math.sin(counter * 2.1) + 1) / 2) * width  / 2) + 16;
-  let recth = Math.floor(((Math.cos(counter * 1.8) + 1) / 2) * height / 2) + 16;
-
-  fill_rect(
-    rust_canvas,
-    rect_colour,
-    rectx,
-    recty,
-    rectx + rectw,
-    recty + recth,
-  );
-
-};
-
-const renderLoop = () => {
-  //debugger;
-
   demo_colourful_rect();
+  demo_hlines();
+  demo_vlines();
 
   drawBuffer();
   counter += 0.025;
